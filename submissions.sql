b4d4e6e7055aed5246b48d51a13492f481ae6602
Chloe <cjzelano@gmail.com>
2026-09-14 22:25:57 -0400

    finished assignment

---
+++ submissions.sql
@@ -0,0 +1,10 @@
+--SELECT author FROM forum_posts WHERE date BETWEEN '2048-04-01' AND '2048-04-30' AND content ILIKE '%Emptystack%'; 
+--SELECT author, content FROM forum_posts WHERE date BETWEEN '2048-04-01' AND '2048-04-30' AND content ILIKE '%Emptystack%';
+--SELECT * FROM forum_accounts WHERE username = 'smart-money-44';
+--SELECT * FROM forum_accounts WHERE last_name = 'Steele'; 
+--SELECT * FROM emptystack_accounts WHERE last_name = 'Steele';
+--node mainframe > username + pw
+--SELECT * FROM emptystack_messages WHERE body ILIKE '%project%';
+--SELECT * FROM emptystack_accounts WHERE username = 'your-boss-99';
+--SELECT * FROM emptystack_projects WHERE code ILIKE 'taxi'; 
+--received project shutdown complete
